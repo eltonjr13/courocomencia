63ffdbb92be9585821ffc1f1e1320f734e9f886b
elton junior <elton.bn.jr@gmail.com>
2025-06-06 04:05:12 +0000

    otimize o site e tire tudo que nao é necessario

--- src/components/couro/FooterInitials.tsx
+++ src/components/couro/FooterInitials.tsx
@@ -1,3 +1,2 @@
-
-// This file is no longer needed and will be deleted.
+// This file is no longer needed and has been emptied as part of optimization.
 // The animated initials have been replaced by the site logo.
--- src/components/intro/IntroOverlay.tsx
+++ src/components/intro/IntroOverlay.tsx
@@ -1,3 +1,2 @@
-
-// This file is no longer needed and will be deleted.
+// This file is no longer needed and has been emptied as part of optimization.
 // The functionality is replaced by SplashScreen.tsx
